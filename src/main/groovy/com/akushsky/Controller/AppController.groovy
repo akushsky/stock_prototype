56f0339c11c4d6266a235fdd88e849235a60366b
akushsky <akushsky@it-universe.ru>
2016-06-07 23:49:22 +0300

    Remove test postconstruct, default values stored in database

--- src/main/groovy/com/akushsky/Controller/AppController.groovy
+++ src/main/groovy/com/akushsky/Controller/AppController.groovy
@@ -30,52 +30,6 @@ class AppController {
     @Autowired
     ExternProviderRepository externProviderRepository;
 
-    @PostConstruct
-    postConstruct() {
-        externProviderRepository.deleteAll()
-        productTypeRepository.deleteAll()
-        productRepository.deleteAll()
-
-        ExternProvider externProvider = new ExternProvider();
-        externProvider.name = "fragrancenet"
-        externProvider.url = "https://fnetdatafeed:datafeed@www.fragrancenet.com/wsdata/datafeed2.txt"
-
-        ExternProvider providerResult = externProviderRepository.save(externProvider)
-
-        ProductType productTypeChanel5 = new ProductType()
-        productTypeChanel5.name = "Chanel #5"
-        productTypeChanel5.type = ProductTypeEnum.BULK;
-
-        ProductType result = productTypeRepository.save(productTypeChanel5)
-
-        Product product2 = new Product()
-        product2.productType = result
-        product2.bulk = 0
-        product2.stockCount = 100
-        product2.stockCountReserved = 0
-
-        Product productResult = productRepository.save(product2)
-
-        ProductType productTypeChanel18 = new ProductType()
-        productTypeChanel18.name = "Chanel #18"
-
-        productTypeChanel18.type = ProductTypeEnum.FULL;
-        if (productTypeChanel18.externProviders == null) {
-            productTypeChanel18.externProviders = new HashSet<ExternProvider>()
-        }
-        productTypeChanel18.externProviders.add(providerResult)
-
-        result = productTypeRepository.save(productTypeChanel18)
-
-        Product product1 = new Product()
-        product1.productType = result
-        product1.bulk = 180
-        product1.stockCount = 10
-        product1.stockCountReserved = 0
-
-        productResult = productRepository.save(product1)
-    }
-
     /**
      * This method will list all existing products.
      */
